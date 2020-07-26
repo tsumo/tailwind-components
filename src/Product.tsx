2a8d50f737f89aa6a20bf864de9d4d6728ff22af
tsumo <tsumo@users.noreply.github.com>
2020-07-26 19:20:20 +0300

    Add example components

---
+++ src/Product.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export const Product = () => (
+  <div className="md:flex">
+    <div className="md:flex-shrink-0">
+      <img
+        className="rounded-lg md:w-56"
+        src="product.webp"
+        alt="Woman paying for a purchase"
+      />
+    </div>
+    <div className="mt-4 md:mt-0 md:ml-6">
+      <div className="uppercase tracking-wide text-sm text-indigo-600 font-bold">
+        Marketing
+      </div>
+      <a
+        href="/"
+        className="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline"
+      >
+        Finding customers for your new business
+      </a>
+      <p className="mt-2 text-gray-600">
+        Getting a new business off the ground is a lot of hard work. Here are
+        five ideas you can use to find your first customers.
+      </p>
+    </div>
+  </div>
+);
