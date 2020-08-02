faaa73ea043dbc547970b7ad00d7c33346022b0b
tsumo <tsumo@users.noreply.github.com>
2020-08-02 12:36:29 +0300

    Compose btn styles

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,16 +1,13 @@
 import React, { FC } from "react";
 import clsx from "clsx";
 
-const buttonStyles =
-  "m-1 px-3 py-2 bg-green-600 hover:bg-green-700 border border-green-800 rounded text-white";
-
 export const Button: FC<React.ButtonHTMLAttributes<HTMLButtonElement>> = (
   props
 ) => {
   const { children, className, ...otherProps } = props;
 
   return (
-    <button className={clsx(buttonStyles, className)} {...otherProps}>
+    <button className={clsx("btn btn-green", className)} {...otherProps}>
       {children}
     </button>
   );
