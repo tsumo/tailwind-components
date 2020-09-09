ea9fda777804959f5c04d3fbb02879f710f0042e
tsumo <tsumo@users.noreply.github.com>
2020-09-09 22:35:27 +0300

    Automatically generate all button combinations

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,36 @@
-import React from "react";
+import React, { ReactElement } from "react";
 import { Button, ButtonProps } from "./components/Button";
 import { Toast } from "./components/Toast";
 import { CircleIcon } from "./icons/Icons";
 
-const buttonProps: ButtonProps[] = [
-  { text: "Click me", color: "green", variant: "outlined" },
-  {
-    text: "Click me",
-    color: "green",
-    variant: "outlined",
-    icon: <CircleIcon />,
-  },
-  { text: "Click me", color: "red", variant: "outlined" },
-  {
-    text: "Click me",
-    color: "red",
-    variant: "outlined",
-    icon: <CircleIcon />,
-  },
-  { text: "Click me", color: "green", variant: "filled" },
-  {
-    text: "Click me",
-    color: "green",
-    variant: "filled",
-    icon: <CircleIcon />,
-  },
-  { text: "Click me", color: "red", variant: "filled" },
-  {
-    text: "Click me",
-    color: "red",
-    variant: "filled",
-    icon: <CircleIcon />,
-  },
-];
+const generateButtons = (): ReactElement[] => {
+  const colors: ButtonProps["color"][] = ["green", "red"];
+  const variants: ButtonProps["variant"][] = ["outlined", "filled"];
+  const icons: ButtonProps["icon"][] = [undefined, <CircleIcon />];
+  const result: ReactElement[] = [];
+  colors.forEach((color) =>
+    variants.forEach((variant) =>
+      icons.forEach((icon) =>
+        result.push(
+          <Button
+            key={`${color}-${variant}${icon ? "-icon" : ""}`}
+            text="Click me"
+            color={color}
+            variant={variant}
+            icon={icon}
+          />
+        )
+      )
+    )
+  );
+  return result;
+};
+const buttons = generateButtons();
 
 export const App = () => {
   return (
     <>
-      {buttonProps.map((props, i) => (
-        <Button key={i} {...props} />
-      ))}
+      {buttons}
       <Toast message="example message" color="green" />
       <Toast
         message="extra long message extra long message extra long message extra long message extra long message extra long message extra long message extra long message extra long message extra long message extra long message extra long message extra long message extra long message extra long message extra long message extra long message extra long message extra long message extra long message extra long message extra long message extra long message extra long message extra long message extra long message extra long message extra long message extra long message extra long message extra long message"
